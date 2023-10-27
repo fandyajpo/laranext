05ddc8a3467186e5c1a6b538b5419a8a23e69f8f
Fandoy <fandyglitch3@gmail.com>
2023-10-27 18:32:47 +0800

    csa

--- resources/views/jadwal.blade.php
+++ resources/views/jadwal.blade.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+  <script src="https://cdn.tailwindcss.com"></script>
+ 
 </head>
 <body>
   <div class="bg-gradient-to-r from-teal-600 to-blue-500 w-full h-32 p-8 border-b border-teal-500 shadow-sm">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+  <script src="https://cdn.tailwindcss.com"></script>
+   
 </head>
 <body>
   <div class="bg-blue-500 w-full h-24"></div>
